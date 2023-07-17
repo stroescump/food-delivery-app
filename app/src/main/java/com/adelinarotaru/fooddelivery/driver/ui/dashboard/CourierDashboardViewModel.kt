10b6fc07969e5bce6f8becb2b5f64744d140febd
Marius Stroescu <m.stroescu@accenture.com>
2023-07-17 09:55:26 +0200

    Working PoC #18 - loading states

--- app/src/main/java/com/adelinarotaru/fooddelivery/driver/ui/dashboard/CourierDashboardViewModel.kt
+++ app/src/main/java/com/adelinarotaru/fooddelivery/driver/ui/dashboard/CourierDashboardViewModel.kt
@@ -7,9 +7,7 @@ import com.adelinarotaru.fooddelivery.shared.models.OrderStatus
 import com.adelinarotaru.fooddelivery.utils.coRunCatching
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class CourierDashboardViewModel(private val courierRepository: CourierRepository) :
@@ -19,31 +17,40 @@ class CourierDashboardViewModel(private val courierRepository: CourierRepository
     val courierOrders = _courierOrders.asSharedFlow()
 
     fun fetchNearbyOrders(orderStatus: OrderStatus) = viewModelScope.launch(Dispatchers.IO) {
+        setLoading(true)
         coRunCatching {
             courierRepository.fetchNearbyOrders(orderStatus)
         }.onSuccess {
+            setLoading(false)
             _courierOrders.tryEmit(it)
         }.onFailure {
+            setLoading(false)
             sendError(it)
         }
     }
 
     fun fetchAcceptedOrders(courierId: String) = viewModelScope.launch(Dispatchers.IO) {
+        setLoading(true)
         coRunCatching {
             courierRepository.fetchAcceptedOrders(courierId)
         }.onSuccess {
+            setLoading(false)
             _courierOrders.tryEmit(it)
         }.onFailure {
+            setLoading(false)
             sendError(it)
         }
     }
 
     fun fetchAllOrders(courierId: String) = viewModelScope.launch(Dispatchers.IO) {
+        setLoading(true)
         coRunCatching {
             courierRepository.fetchAllOrders(courierId)
         }.onSuccess {
+            setLoading(false)
             _courierOrders.tryEmit(it)
         }.onFailure {
+            setLoading(false)
             sendError(it)
         }
     }
